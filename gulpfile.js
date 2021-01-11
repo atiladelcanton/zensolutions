b2923275dadc54b7286738190ff37b9d017ad220
Átila Delcanton Rampazo <atila.rampazo@apektelecom.com>
2021-01-11 08:57:34 -0300

    base preparada

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
+gulp.task('default', watch)
+gulp.task('sass', compileSass);
+
+function compileSass() {
+    return gulp
+        .src("src/scss/**/*.scss")
+        .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
+        .pipe(gulp.dest("src/css"));
+}
+
+function watch() {
+    gulp.watch("src/scss/**/*.scss", compileSass);
+}
